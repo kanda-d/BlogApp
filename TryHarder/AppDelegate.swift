a782d8885cb3524a79167d3f464a6a70b30e75d6
Kanha <k.developer.x@gmail.com>
2020-11-23 10:16:40 +0530

    The change

--- TryHarder/AppDelegate.swift
+++ TryHarder/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    //Actually checking about the code commit
+    //Actually checking about the code commit that's data
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
